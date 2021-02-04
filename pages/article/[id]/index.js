fa4f6f4aeb9cb7b7f49d3e3813c6993029ac974a
Kofi Boakye <66253143+boakyezitek@users.noreply.github.com>
2021-02-04 10:15:50 +0000

    get data by using getStaticProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
 
-function article(props) {
+function article({article}) {
+    const router = useRouter();
+    const {id} = router.query;
+
     return (
-        <div>
-            this is the article
-        </div>
+        <>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br/>
+            <Link href="/">Go Back</Link>
+        </>
     );
+}
+
+// export const  getServerSideProps = async(context) => {
+//     const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//     const article = await res.json();
+
+//     return {
+//         props:{article}
+//     }
+// }
+
+export const  getStaticProps = async(context) => {
+        const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+        const article = await res.json();
+    
+        return {
+            props:{article}
+        }
+    }
+
+export const  getStaticPaths= async(context) => {
+    const res = await fetch (`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json();
+  const ids = articles.map((article)=> article.id);
+  const paths = ids.map((id) => ({params: {id:id.toString()}}));
+    return {
+        paths,
+        fallback:false,
+    }
 }
 
 export default article;
